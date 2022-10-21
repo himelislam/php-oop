240b9fb17fb6762d85b7d3b4e7829c76448f1b6e
himelislam <himel7100@gmail.com>
2022-10-21 15:26:46 +0600

    inheritance

--- LightSwitch.php
+++ LightSwitch.php
@@ -24,3 +24,58 @@ class LightSwitch {
 
 $switch = new LightSwitch();
 $switch->off();
+?>
+<?php
+
+class LightSwitch2 {
+
+    public function __construct()
+    {
+        
+    }
+    
+    public function on(){
+        
+    }
+    public function off(){
+
+    }
+
+    private function connect(){
+
+    }
+    private function active(){
+
+    }
+}
+
+$switch = new LightSwitch();
+$switch->off();
+
+?>
+<?php
+
+class LightSwitch3 {
+
+    public function __construct()
+    {
+        
+    }
+    
+    public function on(){
+        
+    }
+    public function off(){
+
+    }
+
+    private function connect(){
+
+    }
+    private function active(){
+
+    }
+}
+
+$switch = new LightSwitch();
+$switch->off();
