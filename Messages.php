4c4044341c3a4bd9a3340c1a6a0653506401c329
himelislam <himel7100@gmail.com>
2022-10-25 19:08:11 +0600

    namespaceing

--- Messages.php
+++ Messages.php
@@ -21,5 +21,25 @@ $staff = new Staff([$himel]);
 $Rahimshah = new Business($staff);
 
 $Rahimshah->hire(new Person('Julia Aladhin'));
+$Rahimshah->hire(new Person('Julia Aladhin'));
+
+
+$himel = new Person('Himel Islam');
+
+$staff = new Staff([$himel]);
+
+$Rahimshah = new Business($staff);
+
+$Rahimshah->hire(new Person('Julia Aladhin'));
+$Rahimshah->hire(new Person('Julia Aladhin'));
+
+
+$himel = new Person('Himel Islam');
+
+$staff = new Staff([$himel]);
+
+$Rahimshah = new Business($staff);
+
+$Rahimshah->hire(new Person('Julia Aladhin'));
 
 var_dump($Rahimshah->getStaffMembers()); 
