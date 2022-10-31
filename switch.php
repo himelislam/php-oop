edd054ffdd67aa082d08a407059012007cc7ac82
himelislam <himel7100@gmail.com>
2022-10-31 15:34:51 +0600

    switch

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$favColor = '';
+
+switch($favColor){
+    case 'red':
+        echo "Favourite color is Red";
+        break;
+    case 'blue':
+        echo "Favorite coloe is Blue";
+        break;
+    case 'green':
+        echo "Favourite color is Green";
+        break;
+    default:
+        echo "Favorite color is neither red, blue, nor green!";
+}
