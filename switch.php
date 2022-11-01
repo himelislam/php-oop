56b1791733db97ab1bd096098d1d54eb4197ed4f
himelislam <himel7100@gmail.com>
2022-11-01 12:03:33 +0600

    switch 2

--- switch.php
+++ switch.php
@@ -15,3 +15,63 @@ switch($favColor){
     default:
         echo "Favorite color is neither red, blue, nor green!";
 }
+$favColor = '';
+
+switch($favColor){
+    case 'red':
+        echo "Favourite color is Red";
+        break;
+    case 'blue':
+        echo "Favorite coloe is Blue";
+        break;
+    case 'green':
+        echo "Favourite color is Green";
+        break;
+    default:
+        echo "Favorite color is neither red, blue, nor green!";
+}
+$favColor = '';
+
+switch($favColor){
+    case 'red':
+        echo "Favourite color is Red";
+        break;
+    case 'blue':
+        echo "Favorite coloe is Blue";
+        break;
+    case 'green':
+        echo "Favourite color is Green";
+        break;
+    default:
+        echo "Favorite color is neither red, blue, nor green!";
+}
+$favColor = '';
+
+switch($favColor){
+    case 'red':
+        echo "Favourite color is Red";
+        break;
+    case 'blue':
+        echo "Favorite coloe is Blue";
+        break;
+    case 'green':
+        echo "Favourite color is Green";
+        break;
+    default:
+        echo "Favorite color is neither red, blue, nor green!";
+}
+$favColor = '';
+
+switch($favColor){
+    case 'red':
+        echo "Favourite color is Red";
+        break;
+    case 'blue':
+        echo "Favorite coloe is Blue";
+        break;
+    case 'green':
+        echo "Favourite color is Green";
+        break;
+    default:
+        echo "Favorite color is neither red, blue, nor green!";
+}
